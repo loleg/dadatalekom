8e371ccb74b5bfdd2cb789067370c9ed126628bd
Oleg Lavrovsky <oleg@utou.ch>
2016-07-02 16:50:40 +0200

    Tweaks

--- dadatelekom.py
+++ dadatelekom.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[10]:
 
 import numpy as np
 import cairocffi as cairo
@@ -9,11 +9,11 @@ import moviepy.editor as mpy
 import gizeh as gz
 import os, re, random
 
-def gen_gif(term):
-    # In[2]:
 
+# In[11]:
+def gen_gif(term):
     words = []
-    regex = re.compile(r"[^a-zA-Z ]")
+    regex = re.compile(r"[^a-zA-Z \(\)]")
     f = open('dadablah.txt', 'r')
     for line in f.readlines():
         cl = line.lower().split()
@@ -21,19 +21,24 @@ def gen_gif(term):
     bigwords = []
     for w in words:
         w = regex.sub('', w)
-        if len(w) > 1:
-            bigwords.append(w)
+        bigwords.append(w)
+
+    def npshift(seq, n):
+        return np.concatenate((seq[-n:], seq[:-n]))
+
+    # Rotate array by a random shift
+    bigwords = npshift(bigwords, random.randint(0, int(len(bigwords)/2)))
     #bigwords
 
 
-    # In[23]:
+    # In[12]:
 
     # image parameters
     W,H = 320,320
-    D = 5
+    D = 20
 
 
-    # In[89]:
+    # In[14]:
 
     # load background png into an image pattern
     image_surface = cairo.ImageSurface.create_from_png("bg/CanvasByMinimaxDadamax.png")
@@ -44,58 +49,41 @@ def gen_gif(term):
     bg_rect = gz.rectangle(W*2, H*2, fill=gizeh_pattern)
 
     # load additional png
-    random_image_file = os.path.join("img", random.choice(os.listdir("img")))
-    random_image_file
-    image_surface = cairo.ImageSurface.create_from_png(random_image_file)
-    im = 0+np.frombuffer(image_surface.get_data(), np.uint8)
-    im.shape = (image_surface.get_height(), image_surface.get_width(), 4)
-    gizeh_pattern = gz.ImagePattern(im)
-    image_1 = gz.rectangle(W*2, H*2, fill=gizeh_pattern)
+    def get_rand_image():
+        random_image_file = os.path.join("img", random.choice(os.listdir("img")))
+        random_image_file
+        image_surface = cairo.ImageSurface.create_from_png(random_image_file)
+        im = 0+np.frombuffer(image_surface.get_data(), np.uint8)
+        im.shape = (image_surface.get_height(), image_surface.get_width(), 4)
+        gizeh_pattern = gz.ImagePattern(im)
+        return gz.rectangle(W*2, H*2, fill=gizeh_pattern)
 
     start_at = 0
     fr_timer = 0
     chunk_len = 4
+    scenelen = 10
 
-    pad = 50
-    x, y = [], []
-    for i in range(0, chunk_len):
-        x.append(random.randint(pad, W-pad))
-        y.append(random.randint(pad, H-pad))
+    rand_images = [get_rand_image() for i in range(0,D)]
 
     def make_frame(t):
         surface = gz.Surface(W,H, bg_color=(0,0.0,0.0))
         bg_rect.draw(surface)
-        image_1.draw(surface)
+        rand_images[int(t)].draw(surface)
 
-        next_frm = chunk_len * int(t)
-        chosen = bigwords[next_frm : next_frm + chunk_len]
-        if t % 1 == 0:
-            for i in range(0, chunk_len):
-                x[i] = random.randint(pad, W-pad)
-                y[i] = random.randint(pad, H-pad)
+        next_frm = chunk_len * int(t / scenelen)
+        nf_from = int(t) * chunk_len
+        nf_to = nf_from + chunk_len
+        chosen = bigwords[nf_from:nf_to]
 
-        t1 = gz.text(chosen[0],
-                      fontfamily="Impact", fontsize=50,
-                      fill=(1,1,1), angle=-(t/D)*np.pi/2).translate((x[0],y[0]))
-
-        t2 = gz.text(chosen[1],
-                      fontfamily="Impact", fontsize=60,
-                      fill=(1,1,1), xy=(y[0],x[0]))
-
-        angle = 2*np.pi*(1.0+t/D)
-        center = W*(0.5+gz.polar2cart(0.3,angle))
-        t3 = gz.text(chosen[2],
-                      fontfamily="Impact", fontsize=70,
-                      fill=(1,0,0), xy=center)
-
-        t4 = gz.text(chosen[3],
-                      fontfamily="Impact", fontsize=90,
-                      fill=(1,0.8,0.4,t*0.2), xy=(x[2]-t*30,y[2]))
-
-        t2.draw(surface)
-        t4.draw(surface)
-        t3.draw(surface)
-        t1.draw(surface)
+        for i in range(0, chunk_len):
+            fade = 1
+            if i == int((t * 2) % chunk_len): fade = 0.5
+                #((chunk_len - i) / chunk_len) + 1 * (t % scenelen)),
+            ti = gz.text(chosen[i],
+              fontfamily="Impact", fontsize=60,
+              fill=(1, 0.9, 0.6, fade),
+              xy=(W/2,60 + i * 70))
+            ti.draw(surface)
 
         counter = gz.text("%d" % t,
                       fontfamily="Courier New", fontsize=10,
@@ -108,4 +96,4 @@ def gen_gif(term):
     clip.write_gif("output.gif", fps=5, opt="OptimizePlus")
 
 
-    # ![](output.gif..)
+    # ![](output.gif)
